521f9a9d136b96786a1f923e3a4af48fc9e21960
Manuel Paris <37592809+Malteon@users.noreply.github.com>
2022-02-09 17:56:35 +0100

    Update pcb.c

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -1,1 +1,14 @@
 #include "pcb.h"
+#include "asl.h"
+
+static pcb_t pcbFree_table[MAX_PROC];
+static LIST_HEAD(pcbFree_h);
+
+void initPcbs() {
+    for (int i=MAXPROC-1; i<=0; i--) {
+        list_add();
+    }
+}
+
+void freePcb(pcb_t *p) {
+}
