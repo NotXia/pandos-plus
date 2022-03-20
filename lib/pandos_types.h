e7bacbc9e3d43e7ca01d3ba32ec3514eb2552c7f
NotXia <35894453+NotXia@users.noreply.github.com>
2022-03-20 18:38:45 +0100

    Aggiornati header umps3

--- lib/pandos_types.h
+++ lib/pandos_types.h
@@ -16,6 +16,30 @@ typedef signed int   cpu_t;
 typedef unsigned int memaddr;
 
 
+/* Page Table Entry descriptor */
+typedef struct pteEntry_t {
+    unsigned int pte_entryHI;
+    unsigned int pte_entryLO;
+} pteEntry_t;
+
+
+/* Support level context */
+typedef struct context_t {
+    unsigned int stackPtr;
+    unsigned int status;
+    unsigned int pc;
+} context_t;
+
+
+/* Support level descriptor */
+typedef struct support_t {
+    int        sup_asid;                        /* process ID					*/
+    state_t    sup_exceptState[2];              /* old state exceptions			*/
+    context_t  sup_exceptContext[2];            /* new contexts for passing up	*/
+    pteEntry_t sup_privatePgTbl[USERPGTBLSIZE]; /* user page table				*/
+} support_t;
+
+
 /* process table entry type */
 typedef struct pcb_t {
     /* process queue  */
@@ -32,6 +56,15 @@ typedef struct pcb_t {
 
     /* Pointer to the semaphore the process is currently blocked on */
     int *p_semAdd;
+
+    /* Pointer to the support struct */
+    support_t *p_supportStruct;
+
+    /* Indicator of priority; 0 - low, 1 - high */
+    int p_prio;
+
+    /* process id */
+    int p_pid;
 } pcb_t, *pcb_PTR;
 
 
