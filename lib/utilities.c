4752bddc400fe9cfade27f8ea5c1a69b4912ad3b
NotXia <35894453+NotXia@users.noreply.github.com>
2022-04-11 15:12:38 +0200

    Migliorato memcpy

--- lib/utilities.c
+++ lib/utilities.c
@@ -1,9 +1,11 @@
+/*
+    Fonte: https://stackoverflow.com/questions/66809657/undefined-reference-to-memcpy-error-caused-by-ld
+*/
+
 #include "utilities.h"
 
-void *memcpy(void *dest, const void *src, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
+void memcpy(void *dest, const void *src, unsigned int n) {
+    for (unsigned int i=0; i<n; i++) {
         ((char *)dest)[i] = ((char *)src)[i];
     }
 }
--- lib/utilities.h
+++ lib/utilities.h
@@ -1,6 +1,6 @@
 #ifndef PANDOS_UTILITIES_H_INCLUDED
 #define PANDOS_UTILITIES_H_INCLUDED
 
-void *memcpy(void *dest, const void *src, int n);
+void memcpy(void *dest, const void *src, unsigned int n);
 
 #endif
