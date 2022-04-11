b08e818af38154c6f91c2549d7b5a591bceb3a67
NotXia <35894453+NotXia@users.noreply.github.com>
2022-04-11 21:33:39 +0200

    Debugger

--- lib/utilities.h
+++ lib/utilities.h
@@ -3,4 +3,10 @@
 
 void memcpy(void *dest, const void *src, unsigned int n);
 
+#define KLOG_LINES      10
+#define KLOG_LINE_SIZE  30
+
+void klog_print(char *str);
+void klog_print_hex(unsigned int num);
+
 #endif
