501dcef8b8045811b648a24a26141a212d77238a
NotXia <35894453+NotXia@users.noreply.github.com>
2022-08-04 13:19:44 +0200

    Add macro per gestire indirizzi

--- phase3/vmSupport.c
+++ phase3/vmSupport.c
@@ -10,11 +10,13 @@
 
 #define DATA_START_ADDR_OFFSET      0x18
 #define DATA_FILE_SIZE_OFFSET       0x24
-#define FRAME_POOL_START            *((int *)(KERNELSTACK+DATA_START_ADDR_OFFSET)) + *((int *)(KERNELSTACK+DATA_FILE_SIZE_OFFSET))
+#define FRAME_POOL_START            ( *((int *)(KERNELSTACK+DATA_START_ADDR_OFFSET)) + *((int *)(KERNELSTACK+DATA_FILE_SIZE_OFFSET)) )
 
-#define IS_FREE_FRAME(frame)        ((frame)->sw_asid == NOPROC)
-#define FRAME_ADDRESS(index)        (FRAME_POOL_START + (index)*PAGESIZE)
-#define FRAME_NUMBER(frame_addr)    (frame_addr - FRAME_POOL_START) / PAGESIZE
+#define IS_FREE_FRAME(frame)        ( (frame)->sw_asid == NOPROC )
+#define FRAME_NUMBER(frame_addr)    ( (frame_addr - FRAME_POOL_START) / PAGESIZE )                      // Restituisce l'indice del frame dato il suo indirizzo
+#define FRAME_ADDRESS(index)        ( FRAME_POOL_START + (index)*PAGESIZE )                             // Restituisce l'indirizzo del frame dato il suo indice
+#define INDEX_OF_POOL_TABLE(frame)  ( ((memaddr)frame - (memaddr)swap_pool_table) / sizeof(swap_t) )    // Restituisce l'indice del frame dato l'entry della swap pool table
+#define GET_FRAME_ADDRESS(frame)    ( FRAME_ADDRESS(INDEX_OF_POOL_TABLE(frame)) )                       // Restituisce l'indirizzo del frame dato l'entry della swap pool table
 
 #define DISABLE_INTERRUPTS          setSTATUS(getSTATUS() & ~IECON)
 #define ENABLE_INTERRUPTS           setSTATUS(getSTATUS() | IECON)
@@ -70,13 +72,11 @@ void TLBRefillHandler() {
 
 /**
  * @brief Seleziona e restituisce un frame utilizzabile (non necessariamente vuoto).
- * @param frame_address Conterrà l'indirizzo del frame selezionato
  * @returns Entry della swap pool table del frame selezionato.
 */
-static swap_t* _getFrame(memaddr *frame_address) {
+static swap_t* _getFrame() {
     static int to_swap_frame_index = 0;
     swap_t *to_swap_frame_entry = NULL;
-    memaddr to_swap_frame_address;
 
     // Muove il contatore ad un frame libero se esiste (altrimenti fa un giro completo e torna alla posizione originale)
     for (int i=0; i<POOLSIZE; i++) {
@@ -88,12 +88,10 @@ static swap_t* _getFrame(memaddr *frame_address) {
 
     // Estrazione frame
     to_swap_frame_entry = &swap_pool_table[to_swap_frame_index];
-    to_swap_frame_address = FRAME_ADDRESS(to_swap_frame_index);
 
     // Incremento del contatore (prossimo possibile frame da selezionare)
     to_swap_frame_index = (to_swap_frame_index+1) % POOLSIZE;
 
-    *frame_address = to_swap_frame_address;
     return to_swap_frame_entry;
 }
 
@@ -154,9 +152,8 @@ static void _readPageFromFlash(int asid, int page_num, memaddr frame_address) {
 /**
  * @brief Gestisce l'invalidazione di una pagina.
  * @param frame             Puntatore al descrittore del frame che contiene la pagina
- * @param frame_address     Indirizzo di inizio del frame che contiene la pagina
 */
-static void _storePage(swap_t *frame, memaddr frame_address) {
+static void _storePage(swap_t *frame) {
     // Invalidazione della pagina
     DISABLE_INTERRUPTS;
     frame->sw_pte->pte_entryLO = frame->sw_pte->pte_entryLO & ~VALIDON;
@@ -164,21 +161,20 @@ static void _storePage(swap_t *frame, memaddr frame_address) {
     ENABLE_INTERRUPTS;
 
     // Salvataggio della vecchia pagina nel flash drive del processo
-    _writePageToFlash(frame->sw_asid, frame->sw_pageNo, frame_address);
+    _writePageToFlash(frame->sw_asid, frame->sw_pageNo, GET_FRAME_ADDRESS(frame));
 }
 
 /**
  * @brief Gestisce il caricamento di una pagina in memoria.
- * @param pt_entry          Puntatore alla riga nella page table della pagina da caricare
- * @param frame             Puntatore al descrittore del frame che conterrà la pagina
- * @param frame_address     Indirizzo di inizio del frame che conterrà la pagina
+ * @param pt_entry  Puntatore alla riga nella page table della pagina da caricare
+ * @param frame     Puntatore al descrittore del frame che conterrà la pagina
 */
-static void _loadPage(pteEntry_t *pt_entry, swap_t *frame, memaddr frame_address) {
+static void _loadPage(pteEntry_t *pt_entry, swap_t *frame) {
     int asid = ENTRYHI_GET_ASID(pt_entry->pte_entryHI);
     int page_num = _getPageIndex(GET_VPN(pt_entry->pte_entryHI));
 
     // Caricamento della nuova pagina in memoria
-    _readPageFromFlash(asid, page_num, frame_address);
+    _readPageFromFlash(asid, page_num, GET_FRAME_ADDRESS(frame));
 
     // Aggiornamento descrittore del frame
     frame->sw_asid = asid;
@@ -187,7 +183,7 @@ static void _loadPage(pteEntry_t *pt_entry, swap_t *frame, memaddr frame_address
 
     // Aggiornamento della page table
     DISABLE_INTERRUPTS;
-    pt_entry->pte_entryLO = (pt_entry->pte_entryLO & ~ENTRYLO_PFN_MASK) | frame_address | VALIDON;
+    pt_entry->pte_entryLO = (pt_entry->pte_entryLO & ~ENTRYLO_PFN_MASK) | GET_FRAME_ADDRESS(frame) | VALIDON;
     _updateTLB(pt_entry);
     ENABLE_INTERRUPTS;
 }
@@ -208,9 +204,9 @@ static void _TLBInvalidHandler(support_t *support_structure) {
 
     // Manipolazione delle pagine
     if (!IS_FREE_FRAME(new_frame)) {
-        _storePage(new_frame, new_frame_address);
+        _storePage(new_frame);
     }
-    _loadPage(page_pt_entry, new_frame, new_frame_address);
+    _loadPage(page_pt_entry, new_frame);
 
     V(&swap_pool_sem);
     LDST(&support_structure->sup_exceptState[PGFAULTEXCEPT]);
@@ -269,7 +265,7 @@ void freeFrames(int asid) {
 /**
  * @brief Inizializza la tabella delle pagine.
  * @param asid          ASID del processo.
- * @param page_table    Puntatore alla entry nella tabella delle pagine.
+ * @param page_table    Puntatore alla tabella delle pagine del processo.
  * @param tmp_frame     Indirizzo di un frame di appoggio.
 */
 void initPageTable(int asid, pteEntry_t *page_table, memaddr tmp_frame) {
